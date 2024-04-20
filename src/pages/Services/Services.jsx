50f3914ee6aff74a695a0c0775566ee473944b96
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-04-20 17:22:42 +0530

    Services Page Created

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,19 +1,22 @@
 import './Services.css'
 
+
 function Services() {
   return (
     <>
         <div className="services">
-            <div className="heading">
+            <div className="heading flex flex-row justify-start items-center ">
                 <div>
                     <p className="heading2">Services</p>
                 </div>
                 <div>
-                    <p className="heading3 font-normal">Nisal Shiranda Ranasingh</p>
+                    <p className="heading3 font-normal">At our digital marketing agency, we offer a range of services to<br></br> help businesses grow and successed online. Thsese servides are included. </p>
                     
                     
                 </div>
             </div>
+
+           
         </div>
     </>
   )
