2643dbd269d1da356d1527e69b44a3fef45f01cd
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-04-26 19:25:23 +0530

    ss

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -21,48 +21,48 @@ function Services() {
 
         <div className="gridContainer min-[320px]:grid grid-cols-1 sm:grids sm:grid-cols-2 flex place-items-center">
           <div className="gridList1 flex flex-row justify-center items-center lg:w-[25rem]">
-            <div className="box1">
+            <div className="box1 flex flex-col">
               <p className="heading3 font-bold ]">Search engine optimization</p>
-              <div className="flex flex-row justify-start items-center">
+              
                
                 <div><p className="heading3 font-normal ]">Learn More</p></div>
-              </div>
+              
             </div>
             <div className="box2">
               <img className="search" src={Search}></img>
             </div>
           </div>
           <div className="gridList2 flex flex-row justify-center items-center lg:w-[25rem]">
-            <div className="box1">
+            <div className="box1 flex flex-col">
               <p className="heading3 font-bold ]">Search engine optimization</p>
-              <div className="flex flex-row justify-start items-center">
+              
                 
                 <div><p className="heading3 font-normal ]">Learn More</p></div>
-              </div>
+              
             </div>
             <div className="box2 flex justify-center items-center">
               <img className="search" src={Search}></img>
             </div>
           </div>
           <div className="gridList2 flex flex-row justify-center items-center lg:w-[25rem]">
-            <div className="box1">
+            <div className="box1 flex flex-col">
               <p className="heading3 font-bold ]">Search engine optimization</p>
-              <div className="flex flex-row justify-start items-center">
+              
                 
                 <div><p className="heading3 font-normal ]">Learn More</p></div>
-              </div>
+              
             </div>
             <div className="box2 flex justify-center items-center">
               <img className="search" src={Search}></img>
             </div>
           </div>
           <div className="gridList1 flex flex-row justify-center items-center lg:w-[25rem]">
-            <div className="box1">
+            <div className="box1 flex flex-col">
               <p className="heading3 font-bold ]">Search engine optimization</p>
-              <div className="flex flex-row justify-start items-center">
+              
                 
-                <div><p className="heading3 font-normal ]">Learn More</p></div>
-              </div>
+                <p className="heading3 font-normal">Learn More</p>
+              
             </div>
             <div className="box2">
               <img className="search" src={Search}></img>
